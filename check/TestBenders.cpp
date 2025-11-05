fbef194a109c8ac44f6cdd6997342fd1ba1bc954
s2860325 <s2860325@ed.ac.uk>
2025-11-05 13:30:22 +0000

    Removed redundant tests

--- check/TestBenders.cpp
+++ check/TestBenders.cpp
@@ -138,66 +138,6 @@ TEST_CASE("test-division-disjoint", "[highs_benders]") {
     REQUIRE(result.subproblem_rows == std::set<HighsInt>{1});
 }
 
-// TEST_CASE("test-set-master-values", "[highs-benders]") {
-//   /*
-//     Matrix [
-//       1 0
-//       0 1
-//       1 1
-//     ], with first column being a complicating variable
-//   */
-//   std::vector<HighsInt> csr_index {0, 1, 0, 1};
-//   std::vector<double> csr_values = {1, 1, 1, 1};
-//   std::vector<HighsInt> csr_starts {0, 1, 2, 4};
-//   std::set<HighsInt> master_indices {0};
-//   std::vector<double> master_values {1};
-//   HighsLp lp;
-//   lp.num_col_ = 2;
-//   lp.num_row_ = 3;
-//   lp.col_lower_.assign(lp.num_col_, 0);
-//   lp.col_upper_.assign(lp.num_col_, inf);
-//   lp.a_matrix_.start_ = csr_starts;
-//   lp.a_matrix_.index_ = csr_index;
-//   lp.a_matrix_.value_ = csr_values;
-//   auto result = fix_master_variables(lp, master_indices, master_values);
-//   REQUIRE(result);
-//   REQUIRE(lp.col_lower_ == std::vector<double> {1, 0});
-//   REQUIRE(lp.col_upper_ == std::vector<double> {1, inf});
-
-// }
-
-// TEST_CASE("test-set-master-values-invalid-sizes", "[highs-benders]") {
-//   /*
-//     Matrix [
-//       1 0
-//       0 1
-//       1 1
-//     ], with first column being a complicating variable
-//   */
-//   std::vector<HighsInt> csr_index {0, 1, 0, 1};
-//   std::vector<double> csr_values = {1, 1, 1, 1};
-//   std::vector<HighsInt> csr_starts {0, 1, 2, 4};
-//   std::set<HighsInt> master_indices {0};
-//   std::vector<double> master_values {1};
-//   HighsLp lp;
-//   lp.num_col_ = 2;
-//   lp.num_row_ = 3;
-//   lp.col_lower_.assign(lp.num_col_, 0);
-//   lp.col_upper_.assign(lp.num_col_, inf);
-//   lp.a_matrix_.start_ = csr_starts;
-//   lp.a_matrix_.index_ = csr_index;
-//   lp.a_matrix_.value_ = csr_values;
-//   auto result = fix_master_variables(lp, std::set<HighsInt>{0}, std::vector<double>{});
-//   REQUIRE(!result);
-//   REQUIRE(lp.col_lower_ == std::vector<double> {0, 0});
-//   REQUIRE(lp.col_upper_ == std::vector<double> {inf, inf});
-
-//   result = fix_master_variables(lp, std::set<HighsInt>{}, std::vector<double>{1});
-//   REQUIRE(!result);
-//   REQUIRE(lp.col_lower_ == std::vector<double> {0, 0});
-//   REQUIRE(lp.col_upper_ == std::vector<double> {inf, inf});
-// }
-
 TEST_CASE("test-set-master-values-highs-instance", "[highs-benders]") {
   /*
     Matrix [
